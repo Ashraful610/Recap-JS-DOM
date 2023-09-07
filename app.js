c9ffb3b0fbfadabb3bac6b83d919114ddeac37ac
Ashraful610 <ashalam610@gmail.com>
2023-09-07 15:03:07 +0600

    get all elment using id and class selector

---
+++ app.js
@@ -0,0 +1,18 @@
+document.getElementById("container").style.display = "flex";
+document.getElementById("container").style.justifyContent = "space-around";
+
+
+document.getElementById("content-1").style.backgroundColor='orange';
+document.getElementById("content-2").style.backgroundColor='crimson';
+
+document.getElementById("content-3").style.backgroundColor='silver';
+
+document.getElementById("content-4").style.backgroundColor='pink';
+
+document.getElementsByClassName('content').style.width='300px';
+document.getElementsByClassName('content').style.height='200px';
+document.getElementsByClassName('content').style.display='flex';
+document.getElementsByClassName('content').style.justifyContent='center';
+document.getElementsByClassName('content').style.alignItems = 'center'
+
+
